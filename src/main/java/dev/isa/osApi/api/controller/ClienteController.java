63700e693709446a4d743c6e051b06456601b11a
isa <isabellevitorialimadasilva@gmail.com>
2026-03-23 16:01:16 -0300

    Mensagem de Commit V 1.0 - Iniciando projeto de ordem de serviço - Classe cliente configurada - Adicionado dependências Spring Web e Spring Dev Tool - Configurado o endpoint clientes - Configurado retorno em XML

---
+++ src/main/java/dev/isa/osApi/api/controller/ClienteController.java
@@ -0,0 +1,33 @@
+package dev.isa.osApi.api.controller;
+
+
+import dev.isa.osApi.domain.model.Cliente;
+import java.util.ArrayList;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author digma
+ */
+
+@RestController
+public class ClienteController {
+    
+    
+    @GetMapping("/clientes")
+    public ArrayList<Cliente> listas(){
+        ArrayList<Cliente> listaClientes = new ArrayList<Cliente>();
+        listaClientes.add(new Cliente(1, "Kge", "kge@teste.com", "11-99999-9999"));
+        listaClientes.add(new Cliente(2, "Kge", "kge@teste.com", "11-99999-9999"));
+        return listaClientes;
+        
+    }
+    
+    
+}
---
+++ src/main/java/dev/isa/osApi/domain/model/Cliente.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dev.isa.osApi.domain.model;
+
+/**
+ *
+ * @author digma
+ */
+public class Cliente {
+    
+    private long id;
+    private String name;
+    private String email;
+    private String fone;
+
+    public Cliente() {
+    }
+
+    public Cliente(long id, String name, String email, String fone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.fone = fone;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + (int) (this.id ^ (this.id >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        return this.id == other.id;
+    }
+    
+    
+    
+}
